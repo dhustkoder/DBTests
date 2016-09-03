5aabf561f0272bebcef2f98753f8d54f081fe597
dhust <rafaelmoura.dev@gmail.com>
2016-09-03 20:07:03 -0300

    init

---
+++ sqlite3/sqlite.py
@@ -0,0 +1,40 @@
+import os.path
+import sys
+import sqlite3
+
+def connect(db_path):
+    conn = sqlite3.connect(db_path)
+    print("connection success!")
+    return conn
+
+def execute_and_fetch(conn, command):
+    cursor = conn.cursor()
+    cursor.execute(command)
+    fetch = cursor.fetchall()
+    cursor.close()
+    return fetch;
+
+
+def list(conn, rows, table):
+        result = execute_and_fetch(conn, "SELECT " + rows + " FROM " + table)
+        for row in result:
+            print(str(row))
+
+def main():
+    conn = None
+    try:
+        if len(sys.argv) < 3:
+            raise Exception("usage: " + sys.argv[0] + " <db> <table>")
+        elif not os.path.isfile(sys.argv[1]):
+            raise Exception(sys.argv[1] + " is not a db file")
+        conn = connect(sys.argv[1])
+        list(conn, "*", sys.argv[2])
+    except Exception as e:
+        print(str(e))
+    finally:
+        if conn != None:
+            print("closing connection!")
+            conn.close()
+
+
+main()
