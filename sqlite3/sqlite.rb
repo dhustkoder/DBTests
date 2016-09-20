8665f2773e1f72f18ba75daa514c085c5859d7e3
dhust <rafaelmoura.dev@gmail.com>
2016-09-20 12:38:52 -0300

    improving sqlite.rb script

--- sqlite3/sqlite.rb
+++ sqlite3/sqlite.rb
@@ -1,19 +1,20 @@
 #!/bin/ruby
+
 require 'sqlite3'
+
 
 
 def connect(db_file)
 	conn = SQLite3::Database.open(db_file)
-	puts("Connection Success!")
+	puts "Connection Success!"
 	return conn
 end
 
 
 def desconnect(conn)
-	if conn.is_a?(SQLite3::Database)
-		puts("Closing Connection!")
-		conn.close()
-	end
+	return unless conn.is_a?SQLite3::Database
+	puts "Closing Connection!"
+	conn.close()
 end
 
 
@@ -28,36 +29,88 @@ def execute_and_fetch(conn, command)
 		return arr
 	ensure
 		statement.close() if statement
-	end
-	
+	end	
 end
-
 
 
 def list(conn, rows, table)
-	fetch = execute_and_fetch(conn, "SELECT #{rows} FROM #{table}")
-	fetch.each do |row|
-		puts row
-	end
-end
-
-
-def main()
-	begin	
-		if ARGV.length < 2
-			raise "usage: #{$0} <db> <table>"
-		elsif not File.file?(ARGV[0])
-			raise "\'#{ARGV[0]}\' is not a db file"
+	begin
+		fetch = execute_and_fetch(conn, "SELECT #{rows} FROM #{table}")
+		puts "------------ LISTING #{rows} FROM #{table} -------------"
+		fetch.each do |row|
+			puts row
 		end
-		conn = connect(ARGV[0])
-		list(conn, "*", "agend")
-		conn.close()
+		puts "------------------- end -------------------------"
 	rescue Exception => e
 		puts e.message
-	ensure
-		desconnect(conn)
 	end
 end
 
 
-main()
+def update(conn, rows, table)
+	rows_arr = [rows]
+	rows_arr.each do |col|
+		loop do
+			puts "value in '#{col}' youd like to update: (enter nothing to skip)"
+			opt = $stdin.gets.chomp
+			break unless opt != ''
+			puts "enter a new value for #{col} of #{opt}: "
+			new_val = $stdin.gets.chomp
+			conn.execute2("UPDATE #{table} SET #{col} = '#{new_val}' WHERE #{col} = '#{opt}'")
+		end
+	end
+end
+
+
+
+
+
+
+def control(conn)
+	def get_table_n_rows(action)
+		which = ["which table youd like to", "which rows youd like to"]
+		puts "#{which[0]} #{action}: "
+		table = $stdin.gets.chomp
+		puts "#{which[1]} #{action}: "
+		rows = $stdin.gets.chomp
+		return table, rows
+	end
+
+	loop do
+		puts "select an operation: list, update, add, quit: "
+		option = $stdin.gets.chomp.downcase
+		case option
+		when "list"
+			table, rows = get_table_n_rows('list')
+			list(conn, rows, table)
+		when 'update'
+			table, rows = get_table_n_rows('update')
+			update(conn, rows, table)
+		when "quit"
+			break
+		end
+
+	end
+end
+
+
+
+begin
+	if ARGV.length < 1
+		raise "usage: #{$0} <db>"
+	elsif not File.file?(ARGV[0])
+		raise "\'#{ARGV[0]}\' is not a db file"
+	end
+	conn = connect(ARGV[0])
+	control(conn)
+rescue Exception => e
+	puts e.message
+
+ensure
+	desconnect(conn)
+
+end
+
+
+
+
