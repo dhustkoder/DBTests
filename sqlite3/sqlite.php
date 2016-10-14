0bbccc9f42b1a250608cf6db26f74d04c7bab249
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-10-14 06:24:39 -0300

    php agend search

--- sqlite3/sqlite.php
+++ sqlite3/sqlite.php
@@ -1,23 +1,40 @@
 <!DOCTYPE html>
+<?php
+
+if (isset($db) === false) {
+	$db = new SQLite3("agend.db");
+	if ($db === false)
+		die("failed to open database");
+}
+
+
+?>
+
+
 <html>
 	<head>
 		<title>SQLite Agend</title>
 	</head>
 	<body>
-		<h1>Listing Agend Table</h1>
+		<h1>Agend Lookup</h1>
+		<form action="sqlite.php" method="post">
+			<b>Name:</b> <input type="text" name="name"><input type="submit"><br>
+		</form>
 		<?php
-			if ($db = new SQLite3("agend.db")) {
-				$result = $db->query("SELECT * FROM agend");
-				while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-					echo "<h2>Name: {$row["name"]}</h2>";
-					echo "<p>ID: {$row["id"]}</p>";
-					echo "<p>Phone: {$row["phone"]}</p>";
+			if (isset($_POST["name"])) {
+				if ($result = $db->query("SELECT * FROM agend WHERE name = \"{$_POST["name"]}\"")) {
+					while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+						echo "<h4>Name - {$row["name"]}</h4>";
+						echo "<p>ID - {$row["id"]}</p>";
+						echo "<p>Phone - {$row["phone"]}</p>";
+					}
+				} else {
+					echo "<p>{$_POST["name"]} not found!</p>";
 				}
 
-			} else {
-				echo "<p>{$errormsg}</p>";
+				unset($_POST["name"]);
 			}
 		?>
-
 	</body>
 </html>
+
